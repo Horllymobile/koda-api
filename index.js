2224f305059aae3c38cf1c7cb7207912c162a8da
James Abraham Olamide <horlamidex1@gmail.com>
2020-11-21 18:22:42 +0100

    First commit after initializing express

---
+++ index.js
@@ -0,0 +1,12 @@
+const { required } = require("joi");
+
+const express = require('express');
+
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+
+
+app.listen(PORT, console.log(`App running on port ${PORT}`));
